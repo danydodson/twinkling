718a1d513977152538a85cb7c8c81102acae6b46
danydodson <danydodson@gmail.com>
2019-12-29 06:30:53 -0600

    changes

---
+++ frontend/build/precache-manifest.5ea74ef8fe847aea5f4c7a8f7e67c956.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "667a6cddc890896146437b104afa8bf2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "333475bd0a8706a47fc3",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "333475bd0a8706a47fc3",
+    "url": "/static/js/2.1d80b05c.chunk.js"
+  },
+  {
+    "revision": "69a7060c137ed010ccc8833716519768",
+    "url": "/static/js/2.1d80b05c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "81b3ef2c7874d37c008e",
+    "url": "/static/js/main.41598bd3.chunk.js"
+  },
+  {
+    "revision": "0892f646872bcfc90cdb",
+    "url": "/static/js/runtime-main.7c8d7432.js"
+  },
+  {
+    "revision": "79b4d48d465a4ceb49e6747eb33a4404",
+    "url": "/static/media/background.79b4d48d.jpg"
+  },
+  {
+    "revision": "d60d852e9643287d4065be5e71c70b62",
+    "url": "/static/media/background.d60d852e.jpeg"
+  }
+]);
